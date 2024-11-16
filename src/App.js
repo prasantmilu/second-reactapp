7494690581a5b58ec4dde47f87e61e49428c2135
prasantmilu <prasant@ivy-baron.com>
2024-11-16 11:38:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>This heading is creacted in src/app.js and displayed in public index.html</h1>
+    <img src="./images/ks.jpeg"></img>
+    </div>
+  );
+}
+
+export default App;
